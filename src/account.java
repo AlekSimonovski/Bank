c4099387e1fa6c56704cbe6727d08f0ba2645cc1
Александар Симоновски <alek.simonovski02@gmail.com>
2023-04-15 00:37:20 +0200

    Added more functionality to the code.

--- src/account.java
+++ src/account.java
@@ -12,10 +12,45 @@ public class account {
 			password = input.nextLine();										//User inputs pin.
 			if (username.equals(obj1.user) && password.equals(obj1.pass)) {		//Checks if both inputs are the same as the ones set in the "bank" class.
 				System.out.println("You have logged in successfully");				
-				System.out.println(obj1.cardnumber);			
+				System.out.println(obj1.cardnumber);
+				System.out.println("Do you want to continue?");
+				boolean answer;
+				answer = input.nextBoolean();
+				if (answer == true) {
+					System.out.println("Would you like to make a deposit?");
+					boolean deposit;
+					deposit = input.nextBoolean();
+					if (deposit == true) {
+						System.out.println("Type in how much you would like to deposit: ");
+						int deposit1;
+						deposit1 = input.nextInt();
+						obj1.balance = obj1.balance + deposit1;
+						System.out.println("Your new balance is: " + obj1.balance + " MKD.");
+						System.exit(0);
+					}
+					System.out.println("Would you like to make a withdrawal?");
+					boolean withdraw;
+					withdraw = input.nextBoolean();
+					if (withdraw == true) {
+						System.out.println("Type in the amount which you would like to withdraw: ");
+						int withdraw1;
+						withdraw1 = input.nextInt();
+						if (withdraw1 < obj1.balance) {
+							obj1.balance = obj1.balance - withdraw1;
+							System.out.println("Your new balance is: "+ obj1.balance + " MKD.");
+							System.exit(0);
+						}
+						else {
+							System.out.println("You can't withdraw that amount. You only have: "+obj1.balance+" MKD.");
+							System.exit(0);
+						}
+						
+					}
+				}				
 			}
 			else {
 				System.out.println("Error, username or password is incorrect.");
+				System.exit(0);
 			}			
 		}
 }
